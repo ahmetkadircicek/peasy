721cfbf92439e621cac0f5e844514f8c8f0a1378
Ahmet K <104458700+ahmetkadircicek@users.noreply.github.com>
2025-05-18 12:00:00 +0300

    chore: Update provider manager for new services

--- lib/core/init/notifier/provider_manager.dart
+++ lib/core/init/notifier/provider_manager.dart
@@ -1,10 +1,8 @@
 import 'package:peasy/features/cart/viewmodel/cart_view_model.dart';
 import 'package:peasy/features/category/viewmodel/category_view_model.dart';
-import 'package:peasy/features/forgot_password_flow/viewmodel/forgot_password_view_model.dart';
-import 'package:peasy/features/forgot_password_flow/viewmodel/obscure_text_view_model.dart';
-import 'package:peasy/features/forgot_password_flow/viewmodel/otp_view_model.dart';
 import 'package:peasy/features/home/viewmodel/home_view_model.dart';
 import 'package:peasy/features/navigation/viewmodel/navigation_view_model.dart';
+import 'package:peasy/features/nfc/viewmodel/nfc_view_model.dart';
 import 'package:peasy/features/splash/viewmodel/splash_view_model.dart';
 import 'package:provider/provider.dart';
 import 'package:provider/single_child_widget.dart';
@@ -23,9 +21,7 @@ class ProviderManager {
     ChangeNotifierProvider(create: (context) => HomeViewModel()),
     ChangeNotifierProvider(create: (context) => NavigationViewModel()),
     ChangeNotifierProvider(create: (context) => CategoryViewModel()),
-    ChangeNotifierProvider(create: (context) => ForgotPasswordViewModel()),
-    ChangeNotifierProvider(create: (context) => ObscureTextViewModel()),
-    ChangeNotifierProvider(create: (context) => OtpViewModel()),
     ChangeNotifierProvider(create: (context) => CartViewModel()),
+    ChangeNotifierProvider(create: (_) => NFCViewModel()),
   ];
 }
